d5086a2666ed2e9bea47a2880fd3c32ff02e60a5
Dylan Paucar <62946834+dylanp14132@users.noreply.github.com>
2025-11-05 23:12:04 +0000

    LLS Implementation

--- LLS.hpp
+++ LLS.hpp
@@ -14,14 +14,24 @@ public:
     LLS();
 
     // Insertion
-    void push(const T& item) override;
+    void push(const T& item) override {
+        list.addTail();
+    }
 
     // Deletion
-    T pop() override;
+    T pop() override {
+        T temp = list.getTail() 
+        list.removeTail();
+        return temp;
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override {
+        return list.getTail();
+    }
 
     //Getters
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        return list.getCount();
+    }
 };
